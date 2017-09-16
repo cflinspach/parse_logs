48b940357240cfa66cafa06c317c236b7f022bd1
Casey Flinspach <cflinspach@gmail.com>
2017-09-15 17:21:42 -0700

    v0.03 Added Brocade

--- parse_logs.pl
+++ parse_logs.pl
@@ -2,7 +2,7 @@
 # About: Check plugin for Icinga2 to parse a network device's logs.
 #
 # 
-# Version: 0.02
+# Version: 0.03
 #
 # Author: Casey Flinspach
 #         cflinspach@protonmail.com
@@ -13,10 +13,13 @@ use warnings;
 use POSIX qw(strftime);
 use Getopt::Long qw(:config no_ignore_case);
 use IO::Socket;
+use Expect;
 use Net::Telnet;
 use Net::Telnet::Cisco;
 use Net::OpenSSH;
 use File::ReadBackwards;
+#$Net::OpenSSH::debug = -1;
+#$Expect::Debug = 3;
 
 my $search_string = '';
 my $host = '';
@@ -25,8 +28,9 @@ my $password = '';
 my $device_type = '';
 my $fileDir = '/var/www/html/parse_logs/';
 
-my $crit = '';
-my $warn = '';
+my $crit;
+my $warn;
+my $ssh;
 my $result = 0;
 
 GetOptions(
@@ -40,22 +44,12 @@ GetOptions(
         "search|s=s" => \$search_string);
         
 if($help || !$host) {
-        help();
+        Help();
         exit;
-}
-
-sub help { print "check_logs v0.01
-Usage:\n
-check_logs.pl -H [host] -U [username] -P [password] -T [type] -s [search] -w [warn] -c [crit]\n
-
-Possible Types:
-Cisco\n
-Extreme\n";
 }
 
 my $file = $fileDir . $host;
 open my $stdout_fh, '>', $file or die;
-
 
 # Cisco
 if ($device_type =~ 'cisco' || $device_type =~ 'Cisco') {
@@ -65,19 +59,7 @@ if ($device_type =~ 'cisco' || $device_type =~ 'Cisco') {
 	
 	# SSH
 	if ($socket) {
-		my $ssh = Net::OpenSSH->new($host, user=>$username, 
-                                           password=>$password, 
-                                           timeout => 30, 
-                                           master_stdout_fh => $stdout_fh,
-                                           master_stderr_fh => $stdout_fh,
-                                           master_opts => [-o => "KexAlgorithms=+diffie-hellman-group1-sha1",
-                                                           -o => "HostKeyAlgorithms=+ssh-dss",
-                                                           -o => "StrictHostKeyChecking no"]);
-						             
-		if ($ssh->error) {
-			print "Unknown - Unable to connect to remote host: ". $ssh->error . "\n";
-			exit 3;
-		}
+		SSH();
 		
 		# Cisco SSH command
 		my $ssh_session = $ssh->system({stdout_fh=> $stdout_fh}, $cisco_cmd);
@@ -110,9 +92,8 @@ if ($device_type =~ 'cisco' || $device_type =~ 'Cisco') {
 	}
 	foreach my $str (sort keys %count) {
     	$result = $count{$str};
-	}
-	
-};
+	}	
+}
 
 # Extreme Networks
 if ($device_type =~ 'extreme' || $device_type =~ 'Extreme') {
@@ -122,38 +103,27 @@ if ($device_type =~ 'extreme' || $device_type =~ 'Extreme') {
 	
 	# Telenet 
 	if ($socket) {
-		my $telnet = new Net::Telnet(Timeout=>5, Errmode=>'die', Prompt=>'/#/i');
+		my $telnet = new Net::Telnet(Timeout=>30, Errmode=>'die', Prompt=>'/#/i');
 		$telnet->open(Host=>$host);
 		$telnet->login($username, $password);
 		$telnet->cmd('disable clipaging');
 		my @output = $telnet->cmd($extreme_cmd);
 		$telnet->cmd('enable clipaging');
 		print $stdout_fh @output;
-		$telnet->close();
-		
+		$telnet->close();	
 	}
 	
 	# SSH
 	if (!$socket) {
-		my $ssh = Net::OpenSSH->new($host, user=>$username, 
-                                           password=>$password, 
-                                           timeout => 30, 
-                                           master_stdout_fh => $stdout_fh,
-                                           master_stderr_fh => $stdout_fh,
-                                           master_opts => [-o => "KexAlgorithms=+diffie-hellman-group1-sha1",
-                                                           -o => "HostKeyAlgorithms=+ssh-dss",
-                                                           -o => "StrictHostKeyChecking no"]);
-						             
-		$ssh->error and die "Unknown - Unable to connect to remote host: ". $ssh->error;
+		SSH();
 
 		# Cisco SSH command
 		my $ssh_session = $ssh->system({stdout_fh=> $stdout_fh}, $extreme_cmd);
 	}
 	
 	# Open the log file and only read in the first five lines
-    open my $logfile, $file or die "Could not open $stdout_fh: $!";	
-
-
+    open my $logfile, '<', $file or die "Could not open $stdout_fh: $!";	
+	
 	# Count the occurences of the search string
 	my %count;
 	while (my $line = <$logfile>) {
@@ -168,8 +138,61 @@ if ($device_type =~ 'extreme' || $device_type =~ 'Extreme') {
     	$result = $count{$str};
 	}
 
-};
+}
 
+# Brocade/Foundry
+if ($device_type =~ 'Brocade' || $device_type =~ 'brocade') {
+	# Check for SSSH then Telnet
+	my $socket = IO::Socket::INET->new(PeerAddr => $host , PeerPort => 22 , Proto => 'tcp' , Timeout => 1);
+	my $brocade_cmd = "sh log\n";
+	
+	# SSH
+	$ssh = Net::OpenSSH->new($host, user=>$username, 
+                                    password=>$password, 
+                                    timeout => 30,
+                                    master_stderr_discard => 1, 
+                                    master_opts => [-o => "KexAlgorithms=+diffie-hellman-group1-sha1",
+                                                    -o => "HostKeyAlgorithms=+ssh-dss",
+                                                    -o => "StrictHostKeyChecking no"]);
+	if ($ssh->error) {
+		print "Unknown - Unable to connect to remote host: ". $ssh->error . "\n";
+		exit 3;
+	};
+
+	# Brocade/Foundry have an interactive shell, not a command shell
+	my ($pty, $pid) = $ssh->open2pty({tty=>1}, '') or die "unable to run remote command";
+	my $expect = Expect->init($pty);
+	$expect->expect(10, "#");
+	$expect->send("skip\n");
+	$expect->expect(10, "#");
+	$expect->log_file($stdout_fh, "w");
+	$expect->send($brocade_cmd);
+	$expect->expect(10, "#");
+	$expect->log_file(undef);		
+	$expect->send("page-display\n");
+	$expect->expect(10, "#");
+	$expect->send("exit\n");
+	$expect->expect(10, ">");
+	$expect->send("exit\n");
+	
+	# Open the log file and only read in the first five lines
+    open my $logfile, $file or die "Could not open $stdout_fh: $!";	
+
+	# Count the occurences of the search string
+	my %count;
+	while (my $line = <$logfile>) {
+    	foreach my $str ($line =~ /$search_string/g) {
+    		$count{$str}++;
+    	}
+    	last if $. == 18;
+	}
+	close $logfile;
+	
+	foreach my $str (sort keys %count) {
+    	$result = $count{$str};
+	}
+
+}
 
 print "Counted $search_string: $result times\n";
 if ($result < $warn) {
@@ -185,3 +208,36 @@ if ($result < $warn) {
         print "UNKNOWN \n";
         exit 3;
         }
+
+# Subroutines
+########################################################################################################
+# SSH
+sub SSH{
+	$ssh = Net::OpenSSH->new($host, user=>$username, 
+                                    password=>$password, 
+                                    timeout => 30, 
+                                    master_stdout_fh => $stdout_fh,
+                                    master_stderr_fh => $stdout_fh,
+                                    master_opts => [-o => "KexAlgorithms=+diffie-hellman-group1-sha1",
+                                                    -o => "HostKeyAlgorithms=+ssh-dss",
+                                                    -o => "StrictHostKeyChecking no"]);
+	if ($ssh->error) {
+			print "Unknown - Unable to connect to remote host: ". $ssh->error . "\n";
+			exit 3;
+		};
+	
+	return $ssh;
+}        
+ 
+# Help
+sub Help { print "check_logs v0.03
+Usage:\n
+check_logs.pl -H [host] -U [username] -P [password] -T [type] -s [search] -w [warn] -c [crit]\n
+
+Possible Types:
+Cisco\n
+Extreme\n
+Brocade\n
+
+Example: check_logs.pl -H 192.168.0.1 -U Bob -P Pass123 -T cisco -s Gigabit -w 2 - c 5\n";
+}
